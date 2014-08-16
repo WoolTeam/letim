7a52bf2534fdc916eb1a604b36222a7ce94e83ea
zabutok <zabutok@mail.ru>
2014-08-16 12:49:51 +0400

    rate

---
+++ src/Letim/PageBundle/Controller/RateController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Letim\PageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RateController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT
+            p.name,
+            p.duration,
+            p.maxPeople,
+            p.cost,
+            typ.name typename,
+            typ.id typeid
+            FROM LetimCalenderBundle:Plan p
+            LEFT JOIN p.type typ'
+        );
+        $result = $query->getArrayResult();
+        //print_r($result->getName());
+        foreach($result as $plan) {
+//            $arr['name'] = $plan['typename'];
+            //print_r($plan);
+            $arr[$plan['typename']][] = array(
+                'duration' => $plan['duration'],
+                'name' => $plan['name'],
+                'maxPeople' => $plan['maxPeople'],
+                'cost' => $plan['cost']
+            );
+        }
+        return $this->render('LetimPageBundle:Rate:index.html.twig', array(
+            'rate' => $arr
+        ));
+    }
+}
